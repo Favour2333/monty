5926498a47527db162c3fd651e45870d3454d20d
favour2333 <kingsleyfavour999@gmail.com>
2023-03-26 12:49:59 +0000

    monty

--- pall.c
+++ pall.c
@@ -1,41 +1,21 @@
 #include "monty.h"
-
 /**
- *
- *  * f_pall - prints the stack
- *
- *   * @head: stack head
- *
- *    * @counter: counter of the line.
- *
- *     * Return: void
- *
- *     */
-
+ * f_pall - prints the stack
+ * @head: stack head
+ * @counter: counter of the line.
+ * Return: void
+*/
 void f_pall(stack_t **head, unsigned int counter)
-
 {
+	stack_t *h;
+	(void)counter;
 
-		stack_t *h;
-
-			(void)counter;
-
-
-
-				h = *head;
-
-					if (h == NULL)
-
-								return;
-
-						while (h)
-
-								{
-
-											printf("%d\n", h->n);
-
-													h = h->next;
-
-														}
-
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 }
